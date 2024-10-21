bc0ab2a64f0a8613c599805042f032694b164850
maxzim121 <zimensm12@gmail.com>
2024-10-21 13:35:57 +0300

    Initial Commit

---
+++ WeatherAppSUI/WeatherAppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSUIApp.swift
+//  WeatherAppSUI
+//
+//  Created by Maksim Zimens on 21.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
